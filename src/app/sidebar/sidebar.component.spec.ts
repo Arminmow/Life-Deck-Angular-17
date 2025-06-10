0af5d39f97b9dbdca0320f76c3b120c4e0af56dc
Armin <arminmow@gmail.com>
2025-06-10 11:19:57 +0330

    sidebar unit test

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
@@ -8,10 +8,10 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
-    })
-    .compileComponents();
-    
+      imports: [SidebarComponent],
+      providers: [provideAnimations()]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,27 @@ describe('SidebarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have visible initially false', () => {
+    expect(component.visible).toBeFalse();
+  });
+
+  it('Should open drawer', () => {
+    component.open();
+    expect(component.visible).toBeTrue();
+  });
+
+  it('Should close drawer', () => {
+    component.visible = true;
+    component.close();
+    expect(component.visible).toBeFalse();
+  });
+
+  it('should toggle drawer visibility when clicking the button', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+    expect(component.visible).toBeTrue();
   });
 });
